13e94275a6d35d5a9ea870c4dc213c1f5a7f4033
Jaeger <90076904+TheGreatJaeger@users.noreply.github.com>
2025-06-13 03:19:16 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-play_audio("https://github.com/TheGreatJaeger/music.app/raw/refs/heads/main/Kryptonite.mp3")
+play_audio("https://cdn.discordapp.com/attachments/1308153012895744080/1382997516328505436/Kryptonite.mp3")
