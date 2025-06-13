a8ca020080be9818679b3517055a0aca264449e2
Jaeger <90076904+TheGreatJaeger@users.noreply.github.com>
2025-06-13 03:42:20 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,9 @@
-play_audio("https://www.kozco.com/tech/organfinale.mp3")
+
+function music()
+    play_audio("https://www.kozco.com/tech/organfinale.mp3")
+end
+
+get("play").on_click(function()
+    music()
+end)
+
